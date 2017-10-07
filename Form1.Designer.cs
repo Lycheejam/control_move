3dceab959b1df04c2ddb853ea9e0ac688996a405
Lycheejam <32588863+Lycheejam@users.noreply.github.com>
2017-10-07 22:36:27 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace arduino_builder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb1.Location = new System.Drawing.Point(73, 70);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(100, 100);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            this.pb1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pb1_MouseDown);
+            this.pb1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb1_MouseMove);
+            this.pb1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pb1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pb1);
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arduino_builder
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * 1.Drag&Dropで画像を取得する。
+         * 2.Drag&Dropで画像をパネル内で移動させる。
+         */
+        
+        //ドラッグ中かのフラグ
+        bool isDraging = false;
+        //ポインタ宣言
+        //C言語とかで言うポインタじゃなくて
+        //座標位置の格納オブジェクト?変数?
+        //drawingの方のpoint
+        Point? diffPoint = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //ファイルパス取得、画像を表示
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            pb1.Image = Image.FromFile(@fileName[0]);
+        }
+
+        //ドラッグされた対象がファイル化どうかを判断
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }else{
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void pb1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+
+            /*
+             * 参考：フォームやコントロールのマウスポインタ（カーソル）を待機状態にする: .NET Tips: C#, VB.NET 
+             *  >https://dobon.net/vb/dotnet/form/cursorcurrent.html
+             */
+            Cursor.Current = Cursors.Hand;
+            isDraging = true;
+            diffPoint = e.Location;
+            
+        }
+
+        private void pb1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDraging)
+            {
+                return;
+            }
+
+            //移動中の座標を取得？
+            int x = pb1.Location.X + e.X - diffPoint.Value.X;
+            int y = pb1.Location.Y + e.Y - diffPoint.Value.Y;
+
+            //0,0より左と上に行かないよう制限
+            if (x <= 0) x = 0;
+            if (y <= 0) y = 0;
+
+            //フォームの右と下にはみ出さないよう制限
+            if (x >= this.ClientSize.Width - pb1.Width) x = this.ClientSize.Width - pb1.Width;
+            if (y >= this.ClientSize.Height - pb1.Height) y = this.ClientSize.Height - pb1.Height;
+
+            pb1.Location = new Point(x, y);
+        }
+
+        private void pb1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+            isDraging = false;
+
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+            
+        }
+    }
+}
